65c9083334599823a0b76ce661d15154cc05bd9a
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-12-30 11:58:31 -0300

    Implementação de métodos para gerenciar estados do array de usuários

---
+++ src/models/authUser-model.js
@@ -0,0 +1,46 @@
+const uuid = require('uuid').v4
+
+const users = [
+  { id: '1', name: 'Cleyton', cpf: '12345678987', password: '0000', role: 'admin' }
+]
+
+module.exports = {
+  getAllUsers: () => users.map((user) => ({ id: user.id, name: user.name })),
+
+  getUserById: (id) => users.find((user) => user.id === id),
+
+  getUserByCpf: (cpf) => users.find((user) => user.cpf === cpf),
+
+  createStandardUser: (name, cpf, password) => {
+    const newUser = {
+      id: uuid(),
+      name,
+      cpf,
+      password,
+      role: 'standard'
+    }
+    return newUser
+  },
+
+  createAdminUser: (name, cpf, password) => {
+    const newUser = {
+      id: uuid(),
+      name,
+      cpf,
+      password,
+      role: 'admin'
+    }
+    return newUser
+  },
+
+  saveUser: (user) => {
+    users.push(user)
+  },
+
+  deleteUser: (id) => {
+    const userIndex = users.findIndex((u) => u.id === id)
+    if (userIndex === -1) return undefined
+
+    users.slice(userIndex, 1)
+  }
+}
